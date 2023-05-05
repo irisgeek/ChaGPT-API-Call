d8485cd1692acb03db09713ff844e482ca36c567
iris <irisbabecat@gmail.com>
2023-05-04 23:40:45 -0700

    Update python_api.py

--- web/python_api.py
+++ web/python_api.py
@@ -7,6 +7,10 @@ from tools.context import ContextHandler
 from tools.tokennizer import Tokennizer
 
 import time
+import os
+import datetime
+
+log_folder = './chatGPTlogs'
 
 
 class python_api_handler(object):
@@ -34,13 +38,27 @@ class python_api_handler(object):
         self.requestor = OpenAI_Request(keys, model_name,request_address)
 
     def generate_massage(self,user_input):
+        date_today = datetime.datetime.now().strftime("%Y-%m-%d")
+
+        # 获取当前日期 #确保日志文件夹存在，如果不存在则创建一个
+        if not os.path.exists(log_folder):
+            os.makedirs(log_folder) #新建日志文件，文件名为当前日期 
+        log_file_path = os.path.join(log_folder, f'log_{date_today}.txt')
+        
+        #写入日志到文件中
+       
 
         if user_input == "clear":
             self.context_handler.clear()
             print('start a new session')
+            with open(log_file_path, 'a') as f:
+                f.write('start a new session\n')
         else:
             inputs_length = self.tokenizer.num_tokens_from_string(user_input)
             self.context_handler.append_cur_to_context(user_input,inputs_length)
+            print(f'question:{user_input}')
+            with open(log_file_path, 'a') as f:
+                f.write(f'question:{user_input}\n')
 
         st_time = time.time()
 
@@ -49,26 +67,35 @@ class python_api_handler(object):
 
         print(f'post request time cost = {ed_time - st_time}')
 
-        if res.status_code == 200:
 
+        with open(log_file_path, 'a') as f:
+
+            f.write(f'post request time cost = {ed_time - st_time}\n')
+
+        if res.status_code == 200:
+          
             print(res.json())
+           
             response = res.json()['choices'][0]['message']['content']
             # cut \n for show
             response = response.lstrip("\n")
 
             completion_length = res.json()['usage']['completion_tokens']
             total_length = res.json()['usage']['total_tokens']
+           
             print(f"\nresponse : {response}")
-
+            
             self.context_handler.append_cur_to_context(response,completion_length,tag=1)
             if total_length > self.context_max:
                 self.context_handler.cut_context(total_length,self.tokenizer)
 
             print(f'append context time cost = {time.time() - ed_time}')
-
+            with open(log_file_path, 'a') as f:
+                f.write(f"\nresponse : {response}\n")
             return response
         else:
-            print(res.json())
+            with open(log_file_path, 'a') as f:
+                f.write(res.json()+'\n')
             return '!!! The api call is abnormal, please check the backend log'
 
 
